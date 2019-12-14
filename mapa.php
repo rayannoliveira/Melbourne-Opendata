5a5717df815722b7a45da2de5a8884e0ddbd858d
Rayanne Oliveira <rayanneo390@gmail.com>
2019-12-14 11:16:58 -0300

    nova analise

--- mapa.php
+++ mapa.php
@@ -1,31 +1,3 @@
-<?php
-
-    //Recupera do Formulário
-    //$cep = $_POST["txtcep"];
-    //Chamada ao servico REST
-    //URL de servico
-    //Aqui indicamos a URL que iremos utilizar para acessar o serviço
-    $url = "https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json";
-    //Inicializa cURL para uma URL.
-    //Prepara o CURL para criar um conexao que será estabelecida entre a nossa aplicação e o nosso serviço
-    $ch = curl_init($url);
-    //Marca que vai receber string
-    //Existem várias opções de setup, aqui será utilizado apenas que iremos receber uma string
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //Inicia a conexão
-    //Dispara a nossa solicitação, é como se tivessemos dando enter na barra de endereços do navegador
-    $resposta = curl_exec($ch);
-    //Fecha a conexão
-    //Após receber os dados, fechamos a conexao
-    curl_close($ch);
-    //Usando a decodificação em JSON
-    //A forma do PHP trabalhar com JSON é muito simples
-    //Ele transforma os dados recebidos em um objeto ou em uma lista de objetos
-    $listadeobjetos = json_decode($resposta);
-
-    $listadevagas= $listadeobjetos;
-?>
-
 <!DOCTYPE html>
 <html>
 
@@ -38,20 +10,6 @@
     <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
     <link rel="stylesheet" href="assets/css/smoothproducts.css">
-    <meta charset="utf-8">
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
 </head>
 
 <body>
@@ -73,36 +31,11 @@
                 </div>
                 <div class="row align-items-center">
                     <div class="col-md-6">
-                        <div id="map"></div>
-    <script>
-      var map;
-      function initMap() {
-        map = new google.maps.Map(
-            document.getElementById('map'),
-            {center: new google.maps.LatLng(-37.808697589247046, 144.97193891371268), zoom: 12});
-               // Create markers.
-        <?php 
-        foreach ($listadevagas as $e) { ?>
-              var ponto = new google.maps.LatLng(<?php echo $e->lat; ?>,<?php echo $e->lon; ?>);
-              var marker = new google.maps.Marker({
-              position: ponto,//seta posição
-              map: map,//Objeto mapa
-              title:'<?php echo $e->status;?>'//string que será exibida quando passar o mouse no marker
-      //icon: caminho_da_imagem
-  });
-        <?php
-            }
-        ?> 
-      }
-      google.maps.event.addListener(marker, 'click',function(){ alert("Hello World!"); });
-
-    </script>
-                      <iframe allowfullscreen="" frameborder="0" width="100%" height="400" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDbrzMyacs8Gt3ik9pM1NNqpwsQzOmFpo&callback=initMap">
-                        </iframe>
-
-                        </div>
-                    </div>
-                    <div class="col-md-6" style="padding-bottom: 151px;background-color: #ffffff;">
+                        <iframe allowfullscreen="" frameborder="0" width="100%" height="400" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCDbrzMyacs8Gt3ik9pM1NNqpwsQzOmFpo&amp;q=melbourne%2C+Australia&amp;zoom=11">
+                            
+                        </iframe></div>
+                    <div class="col-md-6"
+                        style="padding-bottom: 151px;background-color: #ffffff;">
                         <h3>Data inicial</h3><input class="form-control-lg" type="date">
                         <h3>Data final</h3>
                         <div class="getting-started-info">
@@ -183,9 +116,6 @@
             <p>© 2018 Copyright Text</p>
         </div>
     </footer>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDbrzMyacs8Gt3ik9pM1NNqpwsQzOmFpo&callback=initMap">
-    </script>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
